1334ef81ce9cf07203d86e16ffde62770338d21d
Gustav <Gustav.Viden.6146@student.uu.se>
2019-04-02 16:08:58 +0200

    Merge remote-tracking branch 'remotes/origin/HenrikBranch' into Gustav
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/kvitter/Logic.java
+++ app/src/main/java/com/example/kvitter/Logic.java
@@ -4,7 +4,7 @@ public class Logic {
 
 
     public boolean validateUser(String usrName, String pwd) {
-        if (usrName.contains("gurra")&& pwd.contains("gay")) {
+        if (usrName.contains("Henke")&& pwd.contains("ebög")) {
             return true;
         }
         else {
